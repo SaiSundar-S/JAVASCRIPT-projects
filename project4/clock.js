fa93f1e6dc48778b3f009dfc292faab484454a9c
SAI SUNDAR S <138606156+SaiSundar-S@users.noreply.github.com>
2024-09-25 09:00:17 +0530

    Analog Clock

---
+++ project4/clock.js
@@ -0,0 +1,30 @@
+ let hr=document.getElementById('hour');
+ let min=document.getElementById('min');
+ let sec=document.getElementById('sec');
+ const p=document.querySelector('.time');
+
+ function displayTime(){
+        let date=new Date();
+        
+        let hh=date.getHours();
+        let mm=date.getMinutes();
+        let ss=date.getSeconds();
+
+        let hRotation=30*hh+mm/2;
+        let mRotation=6*mm;
+        let sRotation=6*ss;
+
+        hr.style.transform=`rotate(${hRotation}deg)`;
+        min.style.transform=`rotate(${mRotation}deg)`;
+        sec.style.transform=`rotate(${sRotation}deg)`;
+        
+       
+
+    }
+
+ setInterval(displayTime,1000);
+
+ p.innerText=new Date();
+
+
+ 
